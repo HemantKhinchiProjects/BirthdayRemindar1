b58e8de71b7e1e3c9206ca383e35924d79e4c2c8
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-02-15 18:00:55 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import data from './data';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <main>
+      <section className="container">
+        <h3>birthdays today</h3>
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
